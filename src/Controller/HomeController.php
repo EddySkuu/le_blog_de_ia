d49716c8c848c84042b6db521fdd1c359fe78b28
EddySkuu <edward.jacques12@gmail.com>
2023-06-09 21:14:03 +0200

    Dernier commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\Post\PostRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'home.index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    #[Route('/', name: 'home.index')]
+    public function index(): Response
     {
-        $posts = $postRepository->findPublished();
-
-        return $this->render('home/index.html.twig', [
-            'posts' => $posts
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
